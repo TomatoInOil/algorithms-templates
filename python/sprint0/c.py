fa4b334e2937ba92048ea5c0b38bb39130d2ef0c
Даниил Паутов <danpautoff@yandex.ru>
2022-09-20 20:10:35 +0300

    Решение задачи Спринт0.C. Скользящее среднее.

--- python/sprint0/c.py
+++ python/sprint0/c.py
@@ -1,14 +1,27 @@
 from typing import List, Tuple
 
-def moving_average(arr: List[int], window_size: int) -> List[float]:
-    # Здесь реализация вашего решения
-    pass
 
-def read_input() -> Tuple[List[int], int]:
+def moving_average(n: int, arr: List[int], window_size: int) -> List[float]:
+    result = []
+    current_sum = sum(arr[0:window_size])
+    current_avg = current_sum / window_size
+    result.append(current_avg)
+    for index in range(1, n - window_size + 1):
+        current_sum = (
+            current_sum - arr[index - 1] + arr[index + window_size - 1]
+        )
+        current_avg = current_sum / window_size
+        result.append(current_avg)
+    return result
+
+
+def read_input() -> Tuple[int, List[int], int]:
     n = int(input())
     arr = list(map(int, input().strip().split()))
     window_size = int(input())
-    return arr, window_size
+    return n, arr, window_size
 
-arr, window_size = read_input()
-print(" ".join(map(str, moving_average(arr, window_size))))
+
+if __name__ == "__main__":
+    n, arr, window_size = read_input()
+    print(" ".join(map(str, moving_average(n, arr, window_size))))
