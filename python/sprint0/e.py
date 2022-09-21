ac56b08562e18bd60e387a928aba697c1e63ae6f
Даниил Паутов <danpautoff@yandex.ru>
2022-09-21 11:26:21 +0300

    Решение задачи Спринт0.E. Две фишки - 2.

--- python/sprint0/e.py
+++ python/sprint0/e.py
@@ -1,8 +1,15 @@
 from typing import List, Tuple, Optional
 
+
 def two_sum(arr: List[int], target_sum: int) -> Optional[Tuple[int, int]]:
-    # Здесь реализация вашего решения
-    pass
+    seen = set()
+    for elem in arr:
+        current_difference = target_sum - elem
+        if current_difference in seen:
+            return current_difference, elem
+        seen.add(elem)
+    return None
+
 
 def read_input() -> Tuple[List[int], int]:
     n = int(input())
@@ -10,11 +17,14 @@ def read_input() -> Tuple[List[int], int]:
     target_sum = int(input())
     return arr, target_sum
 
+
 def print_result(result: Optional[Tuple[int, int]]) -> None:
     if result is None:
         print(None)
     else:
         print(" ".join(map(str, result)))
 
-arr, target_sum = read_input()
-print_result(two_sum(arr, target_sum))
+
+if __name__ == "__main__":
+    arr, target_sum = read_input()
+    print_result(two_sum(arr, target_sum))
---
+++ python/sprint0/e_copy.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple, Optional
+
+
+def two_sum(
+    n: int, arr: List[int], target_sum: int
+) -> Optional[Tuple[int, int]]:
+    left = 0
+    right = n - 1
+    while left < right:
+        current_sum = arr[left] + arr[right]
+        if current_sum < target_sum:
+            left += 1
+        if current_sum > target_sum:
+            right -= 1
+        if current_sum == target_sum:
+            return arr[left], arr[right]
+    return None
+
+
+def read_input() -> Tuple[int, List[int], int]:
+    n = int(input())
+    arr = list(map(int, input().strip().split()))
+    target_sum = int(input())
+    return n, arr, target_sum
+
+
+def print_result(result: Optional[Tuple[int, int]]) -> None:
+    if result is None:
+        print(None)
+    else:
+        print(" ".join(map(str, result)))
+
+
+if __name__ == "__main__":
+    n, arr, target_sum = read_input()
+    print_result(two_sum(n, arr, target_sum))
