c0c5eae4c89c8b9cb11bf9b1dc6228317f826479
Даниил Паутов <danpautoff@yandex.ru>
2022-09-21 10:54:50 +0300

    Решение задачи Спринт0.D. Две фишки.

--- python/sprint0/d.py
+++ python/sprint0/d.py
@@ -1,14 +1,22 @@
 from typing import List, Tuple, Optional
 
-def two_sum(arr: List[int], target_sum: int) -> Optional[Tuple[int, int]]:
-    # Здесь реализация вашего решения
-    pass
 
-def read_input() -> Tuple[List[int], int]:
+def two_sum(
+    n: int, arr: List[int], target_sum: int
+) -> Optional[Tuple[int, int]]:
+    for i in range(0, n):
+        for j in range(i + 1, n):
+            if arr[i] + arr[j] == target_sum:
+                return arr[i], arr[j]
+    return None
+
+
+def read_input() -> Tuple[int, List[int], int]:
     n = int(input())
     arr = list(map(int, input().strip().split()))
     target_sum = int(input())
-    return arr, target_sum
+    return n, arr, target_sum
+
 
 def print_result(result: Optional[Tuple[int, int]]) -> None:
     if result is None:
@@ -16,5 +24,7 @@ def print_result(result: Optional[Tuple[int, int]]) -> None:
     else:
         print(" ".join(map(str, result)))
 
-arr, target_sum = read_input()
-print_result(two_sum(arr, target_sum))
+
+if __name__ == "__main__":
+    n, arr, target_sum = read_input()
+    print_result(two_sum(n, arr, target_sum))
