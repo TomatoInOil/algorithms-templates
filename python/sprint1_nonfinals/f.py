64b9fc853b176740fc39771cc9f6decae2cde044
Даниил Паутов <danpautoff@yandex.ru>
2022-09-25 21:15:12 +0300

    Решение задачи Спринт1.F. Палиндром.

--- python/sprint1_nonfinals/f.py
+++ python/sprint1_nonfinals/f.py
@@ -1,5 +1,13 @@
-def is_palindrome(line: str) -> bool:
-    # Здесь реализация вашего решения
-    pass
+import string
 
-print(is_palindrome(input().strip()))
+
+def is_palindrome(line: str) -> bool:
+    table = line.maketrans("", "", string.punctuation + string.whitespace)
+    line = line.translate(table).lower()
+    if line == line[::-1]:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    print(is_palindrome(input().strip()))
