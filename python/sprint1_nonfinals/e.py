e5fba1eba40588e5a6c32e69897b05a5e1db29e1
Даниил Паутов <danpautoff@yandex.ru>
2022-09-25 20:29:35 +0300

    Решение задачи Спринт1.E. Самое длинное слово.

--- python/sprint1_nonfinals/e.py
+++ python/sprint1_nonfinals/e.py
@@ -1,14 +1,22 @@
 def get_longest_word(line: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    words = line.split()
+    words.reverse()
+    word_lengths = sorted(
+        {len(word): word for word in words}.items(), reverse=True
+    )
+    return word_lengths[0]
+
 
 def read_input() -> str:
     _ = input()
     line = input().strip()
     return line
 
-def print_result(result: str) -> None:
-    print(result)
-    print(len(result))
 
-print_result(get_longest_word(read_input()))
+def print_result(result: str) -> None:
+    print(result[1])
+    print(result[0])
+
+
+if __name__ == "__main__":
+    print_result(get_longest_word(read_input()))
