6ece822df1c6d0e7c8da6b6b04a874a41470169a
Даниил Паутов <danpautoff@yandex.ru>
2022-09-25 12:52:10 +0300

    Решение задачи Спринт1.C. Соседи.

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -1,10 +1,23 @@
 from typing import List, Tuple
 
-def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
 
-def read_input() -> Tuple[List[List[int]], int, int]:
+def get_neighbours(
+    n: int, m: int, matrix: List[List[int]], row: int, col: int
+) -> List[int]:
+    result = []
+    if col != m - 1:
+        result.append(matrix[row][col + 1])
+    if col != 0:
+        result.append(matrix[row][col - 1])
+    if row != n - 1:
+        result.append(matrix[row + 1][col])
+    if row != 0:
+        result.append(matrix[row - 1][col])
+    result.sort()
+    return result
+
+
+def read_input() -> Tuple[int, int, List[List[int]], int, int]:
     n = int(input())
     m = int(input())
     matrix = []
@@ -12,7 +25,9 @@ def read_input() -> Tuple[List[List[int]], int, int]:
         matrix.append(list(map(int, input().strip().split())))
     row = int(input())
     col = int(input())
-    return matrix, row, col
+    return n, m, matrix, row, col
 
-matrix, row, col = read_input()
-print(" ".join(map(str, get_neighbours(matrix, row, col))))
+
+if __name__ == "__main__":
+    n, m, matrix, row, col = read_input()
+    print(" ".join(map(str, get_neighbours(n, m, matrix, row, col))))
