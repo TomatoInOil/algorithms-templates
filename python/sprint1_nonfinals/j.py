5e61744367adeb2615ba411b8192f09a6e918732
Даниил Паутов <danpautoff@yandex.ru>
2022-09-26 21:38:47 +0300

    Исправление задачи Спринт1.J. Факторизация.

--- python/sprint1_nonfinals/j.py
+++ python/sprint1_nonfinals/j.py
@@ -3,8 +3,9 @@ from typing import List
 
 def factorize(number: int) -> List[int]:
     root_of_number = int(number ** (0.5) + 2)
-    is_prime = [True] * root_of_number
-    is_prime[0] = is_prime[1] = False
+    is_prime = [False, True] * int(root_of_number / 2 + 1)
+    is_prime[1] = False
+    is_prime[2] = True
     for i in range(3, int(root_of_number ** (0.5) + 2), 2):
         if is_prime[i]:
             for x in range(i * i, root_of_number, i):
