287f26b9ab80db6d736e982488f29bb1fa1e2cce
Даниил Паутов <danpautoff@yandex.ru>
2022-09-26 11:02:02 +0300

    Решение задачи Спринт1.G. Работа из дома.

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,17 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    if number == 0:
+        return "0"
+    remainders = []
+    while number != 0:
+        remainders.append(str(number % 2))
+        number //= 2
+    remainders.reverse()
+    return "".join(remainders)
+
 
 def read_input() -> int:
     return int(input().strip())
 
-print(to_binary(read_input()))
+
+if __name__ == "__main__":
+    print(to_binary(read_input()))
