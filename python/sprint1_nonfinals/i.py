8333f791cacd45fd37d6f63ab025f46349a9757a
Даниил Паутов <danpautoff@yandex.ru>
2022-09-26 15:13:59 +0300

    Решение задачи Спринт1.I. Степень четырёх.

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,14 @@
 def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    remainder = 0
+    if number == 0:
+        return False
+    while number != 1 or remainder != 0:
+        remainder = number % 4
+        if remainder != 0:
+            return False
+        number //= 4
+    return True
 
-print(is_power_of_four(int(input())))
+
+if __name__ == "__main__":
+    print(is_power_of_four(int(input())))
