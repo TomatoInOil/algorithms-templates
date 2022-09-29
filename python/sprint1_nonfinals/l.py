a9c075d0bd899335da3b25fa6af8b92162d7eaf7
Даниил Паутов <danpautoff@yandex.ru>
2022-09-29 13:26:46 +0300

    Решение задачи Спринт1. L. Лишняя буква.

--- python/sprint1_nonfinals/l.py
+++ python/sprint1_nonfinals/l.py
@@ -1,13 +1,23 @@
 from typing import Tuple
 
+
 def get_excessive_letter(shorter: str, longer: str) -> str:
-    # Здесь реализация вашего решения
-    pass
+    short_alphabet = set(shorter)
+    long_alphabet = set(longer)
+    dif = long_alphabet.difference(short_alphabet)
+    if dif:
+        return "".join(dif)
+    for letter in short_alphabet:
+        if shorter.count(letter) != longer.count(letter):
+            return letter
+
 
 def read_input() -> Tuple[str, str]:
     shorter = input().strip()
     longer = input().strip()
     return shorter, longer
 
-shorter, longer = read_input()
-print(get_excessive_letter(shorter, longer))
+
+if __name__ == "__main__":
+    shorter, longer = read_input()
+    print(get_excessive_letter(shorter, longer))
