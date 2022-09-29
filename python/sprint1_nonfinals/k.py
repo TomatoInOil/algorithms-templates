653180e6938d34010f08c9712e0b475af86c2b2e
Даниил Паутов <danpautoff@yandex.ru>
2022-09-29 12:08:32 +0300

    Решение задачи Спринт1.K. Списочная форма.

--- python/sprint1_nonfinals/k.py
+++ python/sprint1_nonfinals/k.py
@@ -1,14 +1,21 @@
 from typing import List, Tuple
+import string
 
-def get_sum(number_list: List[int], k: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
 
-def read_input() -> Tuple[List[int], int]:
-    n = int(input())
-    number_list = list(map(int, input().strip().split()))
+def get_sum(number_list: int, k: int) -> List[str]:
+    result = list(str(number_list + k))
+    return result
+
+
+def read_input() -> Tuple[int, int]:
+    _ = input()
+    number_list = input().strip()
+    table = number_list.maketrans("", "", string.whitespace)
+    number_list = int(number_list.translate(table))
     k = int(input())
     return number_list, k
 
-number_list, k = read_input()
-print(" ".join(map(str, get_sum(number_list, k))))
+
+if __name__ == "__main__":
+    number_list, k = read_input()
+    print(" ".join(get_sum(number_list, k)))
