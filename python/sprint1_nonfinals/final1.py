9b514f61d16b3d7353bcc15cc9d31e9586ea0b85
Даниил Паутов <danpautoff@yandex.ru>
2022-09-30 10:44:37 +0300

    Исправление задачи Спринт1_f.A. Ближайший ноль.

--- python/sprint1_nonfinals/final1.py
+++ python/sprint1_nonfinals/final1.py
@@ -1,10 +1,10 @@
 from typing import List, Tuple
 
 
-def get_distances_to_empty_plot(n: int, house_numbers: List[int]) -> List[int]:
+def get_distances_to_empty_plot(n: int, house_numbers: List[str]) -> List[str]:
     indexes_of_zeros = []
     for index, house in enumerate(house_numbers):
-        if house == 0:
+        if house == "0":
             indexes_of_zeros.append(index)
     for i in range(len(indexes_of_zeros) - 1):
         if indexes_of_zeros[i + 1] - indexes_of_zeros[i] > 1:
@@ -12,7 +12,9 @@ def get_distances_to_empty_plot(n: int, house_numbers: List[int]) -> List[int]:
             right = indexes_of_zeros[i + 1] - 1
             current_distance = 1
             while left <= right:
-                house_numbers[left] = house_numbers[right] = current_distance
+                house_numbers[left] = house_numbers[right] = str(
+                    current_distance
+                )
                 current_distance += 1
                 left += 1
                 right -= 1
@@ -20,30 +22,28 @@ def get_distances_to_empty_plot(n: int, house_numbers: List[int]) -> List[int]:
         current_distance = 1
         right = indexes_of_zeros[0] - 1
         while right >= 0:
-            house_numbers[right] = current_distance
+            house_numbers[right] = str(current_distance)
             current_distance += 1
             right -= 1
     if indexes_of_zeros[-1] != n - 1:
         current_distance = 1
         left = indexes_of_zeros[-1] + 1
         while left <= n - 1:
-            house_numbers[left] = current_distance
+            house_numbers[left] = str(current_distance)
             current_distance += 1
             left += 1
     return house_numbers
 
 
-def read_input() -> Tuple[int, List[int]]:
+def read_input() -> Tuple[int, List[str]]:
     n = int(input())
-    house_numbers = list(map(int, input().split()))
+    house_numbers = input().split()
     return n, house_numbers
 
 
 def main():
     n, house_numbers = read_input()
-    print(
-        " ".join(list(map(str, get_distances_to_empty_plot(n, house_numbers))))
-    )
+    print(" ".join(get_distances_to_empty_plot(n, house_numbers)))
 
 
 if __name__ == "__main__":
