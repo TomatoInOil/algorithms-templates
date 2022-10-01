91b410de9944c3930fdb394d8f695007aef8b0e7
Даниил Паутов <danpautoff@yandex.ru>
2022-10-01 12:28:59 +0300

    Исправления после ревью Спринт1.

--- python/sprint1_nonfinals/final1.py
+++ python/sprint1_nonfinals/final1.py
@@ -1,15 +1,17 @@
 from typing import List, Tuple
 
 
-def get_distances_to_empty_plot(n: int, house_numbers: List[str]) -> List[str]:
+def get_distances_to_empty_plot(
+    street_length: int, house_numbers: List[str]
+) -> List[str]:
     indexes_of_zeros = []
     for index, house in enumerate(house_numbers):
         if house == "0":
             indexes_of_zeros.append(index)
-    for i in range(len(indexes_of_zeros) - 1):
-        if indexes_of_zeros[i + 1] - indexes_of_zeros[i] > 1:
-            left = indexes_of_zeros[i] + 1
-            right = indexes_of_zeros[i + 1] - 1
+    for index in range(len(indexes_of_zeros) - 1):
+        if indexes_of_zeros[index + 1] - indexes_of_zeros[index] > 1:
+            left = indexes_of_zeros[index] + 1
+            right = indexes_of_zeros[index + 1] - 1
             current_distance = 1
             while left <= right:
                 house_numbers[left] = house_numbers[right] = str(
@@ -25,10 +27,10 @@ def get_distances_to_empty_plot(n: int, house_numbers: List[str]) -> List[str]:
             house_numbers[right] = str(current_distance)
             current_distance += 1
             right -= 1
-    if indexes_of_zeros[-1] != n - 1:
+    if indexes_of_zeros[-1] != street_length - 1:
         current_distance = 1
         left = indexes_of_zeros[-1] + 1
-        while left <= n - 1:
+        while left <= street_length - 1:
             house_numbers[left] = str(current_distance)
             current_distance += 1
             left += 1
@@ -36,15 +38,11 @@ def get_distances_to_empty_plot(n: int, house_numbers: List[str]) -> List[str]:
 
 
 def read_input() -> Tuple[int, List[str]]:
-    n = int(input())
+    street_length = int(input())
     house_numbers = input().split()
-    return n, house_numbers
-
-
-def main():
-    n, house_numbers = read_input()
-    print(" ".join(get_distances_to_empty_plot(n, house_numbers)))
+    return street_length, house_numbers
 
 
 if __name__ == "__main__":
-    main()
+    street_length, house_numbers = read_input()
+    print(" ".join(get_distances_to_empty_plot(street_length, house_numbers)))
--- python/sprint1_nonfinals/final2.py
+++ python/sprint1_nonfinals/final2.py
@@ -1,27 +1,25 @@
 from typing import List, Tuple
 
 
-def get_maximum_number_of_points(k: int, keyboard: List[str]) -> int:
-    count_of_keys = [keyboard.count(str(i)) for i in range(1, 10)]
+def get_maximum_number_of_points(
+    number_of_clicks: int, keyboard: List[str]
+) -> int:
+    count_of_keys = [keyboard.count(str(key)) for key in range(1, 10)]
     score = 0
     for count in count_of_keys:
-        if count <= k and count != 0:
+        if count <= number_of_clicks and count != 0:
             score += 1
     return score
 
 
 def read_input() -> Tuple[int, List[str]]:
-    k = 2 * int(input())
+    number_of_clicks = 2 * int(input())
     keyboard = []
     for _ in range(4):
         keyboard += list(input())
-    return k, keyboard
-
-
-def main():
-    k, keyboard = read_input()
-    print(get_maximum_number_of_points(k, keyboard))
+    return number_of_clicks, keyboard
 
 
 if __name__ == "__main__":
-    main()
+    number_of_clicks, keyboard = read_input()
+    print(get_maximum_number_of_points(number_of_clicks, keyboard))
